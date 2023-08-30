8f36d74e89138be02452edca90010b69f3910b0a
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-08-30 16:49:08 +0700

    update cron tab

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import TelegramService from "./services/TelegramService";
+import * as schedule from "node-schedule";
 
 async function main() {
 
@@ -6,6 +7,9 @@ async function main() {
   await telegramService.startListen();
   // await telegramService.sendImage('1740827516', 'http://truyentuan.com/manga2/one-piece/1071/01.jpg');
 
+  const job = schedule.scheduleJob('0 1 * * *', function () {
+    telegramService.sendMessage('1740827516', 'Nộp đơn xin xét tốt nghiệp bổ sung từ 5/9 đến 8/9')
+  });
 }
 
 main().then();
